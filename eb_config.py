7ebb57f0d9675a877fcfe705661e1bd8fd2d6ee3
Christer Enfors <christer.enfors@gmail.com>
2016-10-29 00:07:29 +0200

    Add read_private(), wait argument for recv_message().

--- eb_config.py
+++ eb_config.py
@@ -7,18 +7,31 @@ class Config:
         self.threads = {
             "Twitter"     : None,
             "Telegram"    : None,
+            "IRC"         : None
             }
 
         self.queues = {
             "Main"        : queue.Queue(),
             "TwitterRest" : queue.Queue(),
             "Telegram"    : queue.Queue(),
+            "IRC"         : queue.Queue(),
             }
+        
+    def read_private(self, filename):
+        with open("private/" + filename, "r") as f:
+            return f.read().strip()
         
 
     def send_message(self, recipient, message):
         self.queues[recipient].put(message)
 
 
-    def recv_message(self, recipient):
-        return self.queues[recipient].get()
+    def recv_message(self, recipient, wait = True):
+        if wait:
+            return self.queues[recipient].get()
+        else:
+            try:
+                return self.queues[recipient].get_nowait()
+            except queue.Empty:
+                return None
+
