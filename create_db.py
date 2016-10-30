3bc0dfcef13961410e120ba3eda8121a46da8e0f
Christer Enfors <christer.enfors@gmail.com>
2016-10-30 23:22:40 +0100

    Add IRC_CHANNEL_LOG table creation.

--- create_db.py
+++ create_db.py
@@ -9,11 +9,15 @@ con = sqlite3.connect("enforsbot.db")
 with con:
 
     cur = con.cursor()
-    cur.execute("CREATE   TABLE LOCATION_HISTORY("
+    cur.execute("CREATE TABLE IF NOT EXISTS LOCATION_HISTORY("
                 "USER     TEXT,"
                 "LOCATION TEXT,"
                 "EVENT    TEXT,"
-                "TIME      TIMESTAMP);")
-    
+                "TIME     TIMESTAMP);")
 
-                
+    cur.execute("CREATE TABLE IF NOT EXISTS IRC_CHANNEL_LOG("
+                "USER     TEXT,"
+                "TYPE     TEXT,"
+                "CHANNEL  TEXT,"
+                "MESSAGE  TEXT,"
+                "TIME     TIMESTAMP);")
