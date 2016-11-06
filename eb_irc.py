f6f44f3c8efcc334f5857dc82b31c02a13c71354
Christer Enfors <christer.enfors@gmail.com>
2016-11-06 21:10:24 +0100

    Improve message routing using the new route_msg() function.

--- eb_irc.py
+++ eb_irc.py
@@ -6,20 +6,26 @@ import botymcbotface.irc as irc
 
 prev_msg_text = ""
 
-class IRCThread(eb_thread.Thread):
+class IRCThread(eb_thread.Thread, irc.IRCBot):
 
+    def __init__(self, name, config):
+
+        eb_thread.Thread.__init__(self, name, config)
+
+        self.nickname = self.config.read_private("irc_nickname")
+        self.password = self.config.read_private("irc_password")
+
+        irc.IRCBot.__init__(self, self.nickname, self.password,
+                            debug_level = 1)
+
+        
     def run(self):
         super().run()
         
-        with (self.config.lock):
-            self.nickname = self.config.read_private("irc_nickname")
-            self.password = self.config.read_private("irc_password")
-            self.db = sqlite3.connect("enforsbot.db",
-                                      detect_types = sqlite3.PARSE_DECLTYPES)
+        self.db = sqlite3.connect("enforsbot.db",
+                                  detect_types = sqlite3.PARSE_DECLTYPES)
 
-        self.bot = irc.IRCBot(self.nickname, self.password, debug_level = 1)
-
-        self.bot.connect("irc.freenode.net", "#BotyMcBotface")
+        self.connect("irc.freenode.net", "#BotyMcBotface")
 
         message = eb_message.Message("IRC",
                                      eb_message.MSG_TYPE_THREAD_STARTED)
@@ -37,63 +43,61 @@ class IRCThread(eb_thread.Thread):
 
             if message:
                 if (message.msg_type == eb_message.MSG_TYPE_STOP_THREAD):
-                    self.bot.send("QUIT :Bot stopped.")
-                    self.bot.get_msg(5)
+                    self.send("QUIT :Bot stopped.")
+                    self.get_msg(5)
                     self.stop()
                     return
                 if (message.msg_type == eb_message.MSG_TYPE_USER_MESSAGE):
                     # Send this message as a private message.
                     for line in message.data["text"].rstrip().split("\n"):
-                        self.bot.privmsg(message.data["user"], line)
+                        self.privmsg(message.data["user"], line)
 
             # Check for messages from IRC.
-            msg = self.bot.get_msg(1)
+            msg = self.route_msg(1)
 
-            if not msg:         # If timed out
-                continue
-            
-            self.handle_irc_message(msg)
+            if msg:
+                self.log_irc_msg(msg)
 
 
-    def handle_irc_message(self, msg):
+    # def handle_irc_message(self, msg):
         
-        self.log_irc_message(msg)
+    #     self.log_irc_message(msg)
 
-        if (msg.msg_type == "PRIVMSG" and msg.channel == self.nickname):
-            self.handle_private_message(msg)
-            self.bot.debug_print("Private message: %s->%s: %s" % (msg.sender,
-                                                                  msg.channel,
-                                                                  msg.msg_text),
-                                 1)
+    #     if (msg.msg_type == "PRIVMSG" and msg.channel == self.nickname):
+    #         self.handle_private_message(msg)
+    #         self.debug_print("Private message: %s->%s: %s" % (msg.sender,
+    #                                                           msg.channel,
+    #                                                           msg.msg_text),
+    #                              1)
             
-            print("IRC: Incoming message from %s: '%s'" %
-                  (msg.sender, msg.msg_text))
+    #         print("IRC: Incoming message from %s: '%s'" %
+    #               (msg.sender, msg.msg_text))
 
-        if (msg.msg_type == "PRIVMSG" and msg.channel != self.nickname):
-            self.bot.debug_print("Channel message: %s @ %s: %s" % (msg.sender,
-                                                                   msg.channel,
-                                                                   msg.msg_text),
-                                 1)
-            self.handle_channel_message(msg)
+    #     if (msg.msg_type == "PRIVMSG" and msg.channel != self.nickname):
+    #         self.debug_print("Channel message: %s @ %s: %s" % (msg.sender,
+    #                                                            msg.channel,
+    #                                                            msg.msg_text),
+    #                              1)
+    #         self.handle_channel_message(msg)
             
-        if (msg.msg_type == "JOIN" and
-            msg.channel.lower() == "#botymcbotface"):
+    #     if (msg.msg_type == "JOIN" and
+    #         msg.channel.lower() == "#botymcbotface"):
 
-            if (msg.sender.replace("@", "").lower() in [ "enfors",
-                                                         "botymcbotface",
-                                                         "botymctest",
-                                                         "enforsbot"]):
-                return None
+    #         if (msg.sender.replace("@", "").lower() in [ "enfors",
+    #                                                      "botymcbotface",
+    #                                                      "botymctest",
+    #                                                      "enforsbot"]):
+    #             return None
             
-            message = eb_message.Message("IRC",
-                                         eb_message.MSG_TYPE_NOTIFY_USER,
-                                         { "user": "enfors",
-                                           "text": "We have a visitor in " \
-                                           "#BotyMcBotface: %s." % msg.sender})
-            self.config.send_message("Main", message)
+    #         message = eb_message.Message("IRC",
+    #                                      eb_message.MSG_TYPE_NOTIFY_USER,
+    #                                      { "user": "enfors",
+    #                                        "text": "We have a visitor in " \
+    #                                        "#BotyMcBotface: %s." % msg.sender})
+    #         self.config.send_message("Main", message)
 
 
-    def handle_private_message(self, msg):
+    def on_private_msg(self, msg):
         message = eb_message.Message("IRC",
                                      eb_message.MSG_TYPE_USER_MESSAGE,
                                      { "user"     : msg.sender,
@@ -103,7 +107,7 @@ class IRCThread(eb_thread.Thread):
         self.config.send_message("Main", message)
 
             
-    def handle_channel_message(self, msg):
+    def on_channel_msg(self, msg):
         global prev_msg_text
 
         match = re.search("^s/([^/]+)/([^/]*)/(g?)$",
@@ -112,13 +116,31 @@ class IRCThread(eb_thread.Thread):
         if match:
             old_text = match.group(1)
             new_text = match.group(2)
-            self.bot.privmsg(msg.channel, "Correction: %s" %
-                             prev_msg_text.replace(old_text, new_text))
+            self.privmsg(msg.channel, "Correction: %s" %
+                         prev_msg_text.replace(old_text, new_text))
         else:
             prev_msg_text = msg.msg_text
-            
 
-    def log_irc_message(self, msg):
+    def on_join_msg(self, msg):
+        if msg.channel.lower != "#botymcbotface":
+            return None
+        
+        if (msg.sender.replace("@", "").lower() in [ "enfors",
+                                                     "botymcbotface",
+                                                     "botymctest",
+                                                     "enforsbot"]):
+            return None
+            
+        message = eb_message.Message("IRC",
+                                     eb_message.MSG_TYPE_NOTIFY_USER,
+                                     { "user": "enfors",
+                                       "text": "We have a visitor in " \
+                                       "#BotyMcBotface: %s." % msg.sender})
+        self.config.send_message("Main", message)
+
+
+
+    def log_irc_msg(self, msg):
 
         with self.config.lock, self.db:
 
