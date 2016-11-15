0cab3f5aece6ca3a3be2c8aaf50fa0e606e4035e
Christer Enfors <christer.enfors@gmail.com>
2016-11-15 13:26:30 +0100

    PEP8 corrections.

--- enforsbot.py
+++ enforsbot.py
@@ -2,21 +2,21 @@
 "enforsbot.py by Christer Enfors (c) 2015, 2016"
 
 
-import eb_config, eb_thread, eb_queue, eb_message, eb_twitter, eb_telegram
+import eb_config, eb_message, eb_twitter, eb_telegram
 import eb_irc
-import time, datetime, threading, re, socket, subprocess, sqlite3, sys
+import datetime, re, socket, subprocess, sqlite3
 
 #twitter_thread = eb_twitter.TwitterThread()
 
 SYSCOND_DIR = "/home/enfors/syscond"
 
 
-class EnforsBot:
+class EnforsBot(object):
     "The main class of the application."
 
     def __init__(self):
         self.config = eb_config.Config()
-        
+
         # Responses are regexps.
         self.responses = {
             "ip"                 : self.respond_ip,
@@ -52,10 +52,10 @@ class EnforsBot:
         }
 
         self.location = None
-        self.arrived  = False
+        self.arrived = False
 
-        self.db = sqlite3.connect("enforsbot.db",
-                                  detect_types = sqlite3.PARSE_DECLTYPES)
+        self.database = sqlite3.connect("enforsbot.db",
+                                        detect_types=sqlite3.PARSE_DECLTYPES)
 
 
 
@@ -72,9 +72,11 @@ class EnforsBot:
             while True:
                 message = self.config.recv_message("Main")
 
-                print("Main: Incoming message from thread %s..." % message.sender)
-            
-                if message.msg_type == eb_message.MSG_TYPE_THREAD_STARTED:
+                print("Main: Incoming message from thread %s..." % \
+                      message.sender)
+
+                if message.msg_type == \
+                   eb_message.MSG_TYPE_THREAD_STARTED:
                     print("Thread started: %s" % message.sender)
                     self.config.set_thread_state(message.sender,
                                                  "running")
@@ -85,8 +87,8 @@ class EnforsBot:
                                                  "stopped")
 
                 elif message.msg_type == eb_message.MSG_TYPE_USER_MESSAGE:
-                    self.handle_incoming_user_message(message,
-                                                      self.response_threads[message.sender])
+                    self.handle_incoming_user_message(message, \
+                                        self.response_threads[message.sender])
 
                 elif message.msg_type == eb_message.MSG_TYPE_LOCATION_UPDATE:
                     self.handle_incoming_location_update(message)
@@ -94,11 +96,12 @@ class EnforsBot:
                 elif message.msg_type == eb_message.MSG_TYPE_NOTIFY_USER:
                     self.handle_incoming_notify_user(message)
                 else:
-                    print("Unsupported incoming message type: %d" % message.msg_type)
+                    print("Unsupported incoming message type: %d" % \
+                          message.msg_type)
         except (KeyboardInterrupt, SystemExit):
             self.stop_all_threads()
             return
-        
+
 
     def start_all_threads(self):
         "Start all necessary threads."
@@ -126,13 +129,13 @@ class EnforsBot:
 
 
     def stop_all_threads(self):
-
+        "Stop all threads."
         print("") # Add a newline to get away from "^C" on screen
-        
+
         with self.config.lock:
 
-            threads_to_stop = [ thread for thread in self.config.threads if
-                                self.config.thread_states[thread] == "running" ]
+            threads_to_stop = [thread for thread in self.config.threads if
+                               self.config.thread_states[thread] == "running"]
 
             print("Stopping threads: %s" % threads_to_stop)
 
@@ -145,26 +148,27 @@ class EnforsBot:
 
 
     def stop_thread(self, thread):
-
+        "Stop one specific thread."
         message = eb_message.Message("Main",
-                                     eb_message.MSG_TYPE_STOP_THREAD, { } )
+                                     eb_message.MSG_TYPE_STOP_THREAD, {})
         self.config.send_message(thread, message)
 
         self.config.threads[thread].join()
-        
+
 
     def handle_incoming_user_message(self, message, response_thread):
+        "Hande an incoming message of type USER."
         user = message.data["user"]
         text = message.data["text"]
-        
+
         #print("Main: Message from %s: '%s'" % (user, text))
 
         response = "I'm afraid I don't understand."
 
         # If this is an IRC message:
-        if (response_thread == "IRC"):
-            msg_type = message.data["msg_type"]
-            channel  = message.data["channel"]
+        if response_thread == "IRC":
+            #msg_type = message.data["msg_type"]
+            channel = message.data["channel"]
 
             # But don't respond unless it's a private message.
             if channel.lower() != "enforsbot" and \
@@ -172,15 +176,15 @@ class EnforsBot:
                 return None
 
             # We should't try to respond to NickServ.
-            if (user.lower() == "nickserv"):
+            if user.lower() == "nickserv":
                 return None
-            
+
         text = text.lower()
 
         for pattern in self.responses.keys():
-            p = re.compile(pattern)
+            pat = re.compile(pattern)
 
-            if p.match(text):
+            if pat.match(text):
                 response = self.responses[pattern]
 
                 if callable(response):
@@ -190,45 +194,48 @@ class EnforsBot:
         if response is not None:
             message = eb_message.Message("Main",
                                          eb_message.MSG_TYPE_USER_MESSAGE,
-                                         { "user" : user,
-                                           "text" : response })
+                                         {"user" : user,
+                                          "text" : response})
             self.config.send_message(response_thread, message)
 
 
     def respond_ip(self, message):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("gmail.com", 80)) # I'm abusing gmail. I'm sure it can take it.
-        response = "I'm currently running on IP address %s." % s.getsockname()[0]
-        s.close()
+        "Return our local IP address."
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.connect(("gmail.com", 80)) # I'm abusing gmail.
+                                        # I'm sure it can take it.
+        response = "I'm currently running on IP address %s." % \
+                   sock.getsockname()[0]
+        sock.close()
 
         return response
 
 
     def handle_incoming_location_update(self, message):
-        user     = "Enfors" # Hardcoded for now. Sue me.
+        user = "Enfors" # Hardcoded for now. Sue me.
         location = message.data["location"]
-        arrived  = message.data["arrived"]
+        arrived = message.data["arrived"]
 
         print("Updating location: [%s:%s]" % (location, str(arrived)))
-        
-        with self.config.lock, self.db:
 
-            cur = self.db.cursor()
+        with self.config.lock, self.database:
+
+            cur = self.database.cursor()
 
             if arrived:
 
                 self.location = location
-                self.arrived  = True
+                self.arrived = True
 
                 cur.execute("insert into LOCATION_HISTORY "
                             "(user, location, event, time) values "
                             "(?, ?, 'arrived', ?)",
                             (user, location, datetime.datetime.now()))
-                
+
                 print("Main: Location updated: %s" % self.location)
-                
+
             else: # if leaving
-                
+
                 # If leaving the location I'm currently at (sometimes
                 # the "left source" message arrives AFTER "arrived at
                 # destination" message), skipping those.
@@ -238,7 +245,7 @@ class EnforsBot:
                                 "(user, location, event, time) values "
                                 "(?, ?, 'left', ?)",
                                 (user, location, datetime.datetime.now()))
-                    
+
                     print("Main: Location left: %s" % location)
                     self.arrived = False
 
@@ -246,15 +253,17 @@ class EnforsBot:
 
 
     def handle_incoming_notify_user(self, message):
+        "Send notification message through Twitter."
 
         out_message = eb_message.Message("Main",
                                          eb_message.MSG_TYPE_USER_MESSAGE,
-                                         { "user": message.data["user"],
-                                           "text": message.data["text"] })
+                                         {"user": message.data["user"],
+                                          "text": message.data["text"]})
         self.config.send_message("TwitterRest", out_message)
 
-    
+
     def respond_location(self, message):
+        "Return our location."
         #if not self.location:
         #    return "There whereabouts of Enfors are currently unknown."
 
@@ -263,15 +272,12 @@ class EnforsBot:
         #else:
         #    return "Enfors was last seen leaving %s."     % self.location
 
-        with self.db:
+        with self.database:
 
-            try:
-                cur = self.db.cursor()
-            
-                cur.execute("select * from LOCATION_HISTORY "
-                            "order by ROWID desc limit 1")
-            except:
-                return "It seems someone *ahem* has forgotten to create my database."
+            cur = self.database.cursor()
+
+            cur.execute("select * from LOCATION_HISTORY "
+                        "order by ROWID desc limit 1")
 
             try:
                 (user, location, event, timestamp) = cur.fetchone()
@@ -279,20 +285,22 @@ class EnforsBot:
                 return "I have no information on that."
 
             if event == "arrived":
-                return "%s %s at %s %s." % (user, event, location,
-                                            self.get_datetime_diff_string(timestamp,
-                                                                          datetime.datetime.now()))
+                return "%s %s at %s %s." % (user, event, location, \
+                    self.get_datetime_diff_string(timestamp,
+                                                  datetime.datetime.now()))
             else:
-                return "%s %s %s %s." % (user, event, location,
-                                         self.get_datetime_diff_string(timestamp,
-                                                                       datetime.datetime.now()))
+                return "%s %s %s %s." % (user, event, location, \
+                    self.get_datetime_diff_string(timestamp,
+                                                  datetime.datetime.now()))
 
 
     def respond_syscond(self, message):
+        "Return the SysCond status of the host."
         return self.check_syscond()
 
 
     def respond_status(self, message):
+        "Return threads status."
         output = ""
 
         for thread in self.config.threads:
@@ -300,12 +308,13 @@ class EnforsBot:
                                     self.config.get_thread_state(thread))
 
         return output
-    
+
 
     def check_syscond(self):
+        "Check the SysCond status of the host."
         try:
-            syscond_output = subprocess.Popen(["syscond", "status", "-n"],
-                                              stdout=subprocess.PIPE).communicate()[0]
+            syscond_output = subprocess.Popen(["syscond", "status", "-n"], \
+                stdout=subprocess.PIPE).communicate()[0]
 
             return syscond_output.decode("utf-8")
         except FileNotFoundError:
@@ -313,7 +322,7 @@ class EnforsBot:
 
 
     def get_datetime_diff_string(self, d1, d2):
-
+        "Return the diff between to datetimes, in short format."
         if d1 > d2:
             return "in the future"
 
@@ -322,39 +331,48 @@ class EnforsBot:
         total_seconds = diff.total_seconds()
 
         minutes = total_seconds // 60
-        hours   = total_seconds // 60 // 60
-        days    = total_seconds // 60 // 60 // 24
+        hours = total_seconds // 60 // 60
+        days = total_seconds // 60 // 60 // 24
 
         if days:
             hours -= (days * 24)
-            
-            return "%d %s, %d %s ago" % (days,    self.get_possible_plural("day",    days),
-                                         hours,   self.get_possible_plural("hour",   hours))
+
+            return "%d %s, %d %s ago" % (days,
+                                         self.get_possible_plural("day", days),
+                                         hours,
+                                         self.get_possible_plural("hour",
+                                                                  hours))
         elif hours:
             minutes -= (hours * 60)
 
-            return "%d %s, %d %s ago" % (hours,   self.get_possible_plural("hour",   hours),
-                                         minutes, self.get_possible_plural("minute", minutes))
-            
+            return "%d %s, %d %s ago" % (hours,
+                                         self.get_possible_plural("hour",
+                                                                  hours),
+                                         minutes,
+                                         self.get_possible_plural("minute",
+                                                                  minutes))
+
         elif minutes:
-            return "%d %s ago" % (minutes, self.get_possible_plural("minute", minutes))
-        
+            return "%d %s ago" % (minutes, self.get_possible_plural("minute",
+                                                                    minutes))
+
         else:
             return "just now"
 
 
     def get_possible_plural(self, word, num):
+        "Return word+s if num is plural, otherwise word."
         if num == 1:
             return word
         else:
             return word + "s"
-        
-            
+
+
 def main():
     "Start the application."
     app = EnforsBot()
     app.start()
-    
+
 
 if __name__ == "__main__":
     main()
