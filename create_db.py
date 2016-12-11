99530000e63ec6f00208f857e5cb501249259142
Christer Enfors <christer.enfors@gmail.com>
2016-12-11 20:11:44 +0100

    Add table USER.

--- create_db.py
+++ create_db.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3
-import sys
 
 con = sqlite3.connect("enforsbot.db")
 
@@ -10,14 +9,24 @@ with con:
 
     cur = con.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS LOCATION_HISTORY("
-                "USER     TEXT,"
-                "LOCATION TEXT,"
-                "EVENT    TEXT,"
-                "TIME     TIMESTAMP);")
+                "USER        TEXT,"
+                "LOCATION    TEXT,"
+                "EVENT       TEXT,"
+                "TIME        TIMESTAMP);")
 
     cur.execute("CREATE TABLE IF NOT EXISTS IRC_CHANNEL_LOG("
-                "USER     TEXT,"
-                "TYPE     TEXT,"
-                "CHANNEL  TEXT,"
-                "MESSAGE  TEXT,"
-                "TIME     TIMESTAMP);")
+                "USER        TEXT,"
+                "TYPE        TEXT,"
+                "CHANNEL     TEXT,"
+                "MESSAGE     TEXT,"
+                "TIME        TIMESTAMP);")
+
+    cur.execute("CREATE TABLE IF NOT EXISTS USER("
+                "USERID      INTEGER PRIMARY KEY AUTOINCREMENT,"
+                "NAME        TEXT,"
+                "PASSWORD    TEXT,"
+                "TWITTER_ID  TEXT,"
+                "TELEGRAM_ID TEXT,"
+                "IRC_ID      TEXT,"
+                "CREATED     TIMESTAMP);")
+
