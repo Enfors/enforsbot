ca255c42039677af699b7592aea0aa20024e7ee2
Christer Enfors <christer.enfors@gmail.com>
2017-10-13 13:12:51 +0200

    PEP8 corrections.

--- eb_message.py
+++ eb_message.py
@@ -1,9 +1,9 @@
-MSG_TYPE_THREAD_STARTED     = 1
-MSG_TYPE_STOP_THREAD        = 2
-MSG_TYPE_THREAD_STOPPED     = 3
-MSG_TYPE_USER_MESSAGE       = 4
-MSG_TYPE_LOCATION_UPDATE    = 5
-MSG_TYPE_NOTIFY_USER        = 6
+MSG_TYPE_THREAD_STARTED = 1
+MSG_TYPE_STOP_THREAD = 2
+MSG_TYPE_THREAD_STOPPED = 3
+MSG_TYPE_USER_MESSAGE = 4
+MSG_TYPE_LOCATION_UPDATE = 5
+MSG_TYPE_NOTIFY_USER = 6
 
 VALID_MSG_TYPES = [
     MSG_TYPE_THREAD_STARTED,
@@ -14,33 +14,32 @@ VALID_MSG_TYPES = [
     MSG_TYPE_NOTIFY_USER,
     ]
 
-example_user_messages_data= {
-    # Incoming or outgoing depends on the context. Could be from owner to bot or
-    # bot to owner (or perhaps from bot to other user)
-    "user" : "username",
-    "text" : "foo",
+EXAMPLE_USER_MESSAGES_DATA = {
+    # Incoming or outgoing depends on the context. Could be from owner to
+    # bot or bot to owner (or perhaps from bot to other user)
+    "user": "username",
+    "text": "foo",
 }
 
 example_location_update_data = {
-    "location" : "home",
-    "arrived"  : True, # True = arrived, False = left
+    "location": "home",
+    "arrived": True,  # True = arrived, False = left
 }
+
 
 class MessageTypeInvalid(Exception):
     pass
 
+
 class Message:
-    def __init__(self, sender, msg_type, data = None):
+    def __init__(self, sender, msg_type, data=None):
 
         if msg_type not in VALID_MSG_TYPES:
             raise MessageTypeInvalid
-        
-        self.sender   = sender
+
+        self.sender = sender
         self.msg_type = msg_type
-        self.data     = data
+        self.data = data
 
     def __repr__(self):
         return "Message type: %d, sender: %s" % (self.msg_type, self.sender)
-        
-
-    
