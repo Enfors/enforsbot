f9500486d13aea85aec02ff8a06fca1ebec9286a
Christer Enfors <christer.enfors@gmail.com>
2017-10-13 13:45:34 +0200

    PEP8 corrections.

--- eb_telegram.py
+++ eb_telegram.py
@@ -1,30 +1,36 @@
-import eb_thread, eb_message
+import eb_message
+import eb_thread
 
-import telepot, random
+import random
+import telepot
+import time
+
 
 def read_private(filename):
+    "Return contents of file in private/ dir."
     with open("private/" + filename, "r") as f:
         return f.read().replace('\n', '')
 
-TOKEN  = read_private("telegram_token")
 
+TOKEN = read_private("telegram_token")
 bot_name = "Enfors_bot"
+
 
 class TelegramThread(eb_thread.Thread):
     def __init__(self, name, config):
         super().__init__(name, config)
 
-        self.state = None # Temp. Remove later.
+        self.state = None  # Temp. Remove later.
 
-        
     def run(self):
         super().run()
         with self.config.lock:
             self.bot = telepot.Bot(TOKEN)
 
         self.bot.message_loop(self.handle_message)
-            
-        message = eb_message.Message("Telegram", eb_message.MSG_TYPE_THREAD_STARTED)
+
+        message = eb_message.Message("Telegram",
+                                     eb_message.MSG_TYPE_THREAD_STARTED)
         self.config.send_message("Main", message)
 
         while True:
@@ -34,7 +40,7 @@ class TelegramThread(eb_thread.Thread):
                 if (message.msg_type == eb_message.MSG_TYPE_STOP_THREAD):
                     self.stop()
                     return
-                
+
                 if message.msg_type == eb_message.MSG_TYPE_USER_MESSAGE:
                     self.bot.sendMessage(message.data["user"],
                                          message.data["text"])
@@ -43,10 +49,8 @@ class TelegramThread(eb_thread.Thread):
                 self.config.set_thread_state("Telegram", "exception")
                 time.sleep(60)
 
-        
-
-
     def handle_message(self, msg):
+        "Take care of a message from the user."
         content_type, chat_type, chat_id = telepot.glance(msg)
 
         if content_type == "text":
@@ -58,7 +62,7 @@ class TelegramThread(eb_thread.Thread):
 
             if text.lower().startswith("@enfors_bot "):
                 text = text[12:]
-            
+
             print("Telegram: Incoming message from %s(%d): '%s'" %
                   (msg["from"]["first_name"], user, text))
 
@@ -71,44 +75,41 @@ class TelegramThread(eb_thread.Thread):
             # Send the message to the main thread.
             message = eb_message.Message("Telegram",
                                          eb_message.MSG_TYPE_USER_MESSAGE,
-                                         { "user": msg["chat"]["id"],
-                                           "text": text })
+                                         {"user": msg["chat"]["id"],
+                                          "text": text})
             self.config.send_message("Main", message)
         else:
             print("Incoming %s message, ignoring.", content_type)
 
-
-
     def handle_telegram_message(self, text, user):
-        if text == "rps": # Rock paper scissors
+        """Handle telegram-specific messages."""
+        if text == "rps":  # Rock paper scissors
             self.state = "rps"
 
-            show_keyboard = { "keyboard" : [[
+            show_keyboard = {"keyboard": [[
                 "rock", "paper", "scissors"
             ]]}
-            
+
             self.bot.sendMessage(user, "You want to play "
                                  "rock, paper, scissors?\n"
                                  "Okay, make your choice:",
-                            reply_markup = show_keyboard)
+                                 reply_markup=show_keyboard)
 
             return True
 
-        if self.state == "rps" and text in [ "rock", "paper", "scissors" ]:
+        if self.state == "rps" and text in ["rock", "paper", "scissors"]:
             self.finish_playing_rps(text, user)
             return True
 
-        
         return False
 
-
-
     def finish_playing_rps(self, user_choice, user):
-        my_choice = [ "rock", "paper", "scissors" ][random.randint(0, 2)]
+        """Finish a session of RPS."""
+        my_choice = ["rock", "paper", "scissors"][random.randint(0, 2)]
 
-        hide_keyboard = { "hide_keyboard" : True }
+        hide_keyboard = {"hide_keyboard": True}
         self.bot.sendMessage(user, "I choose %s" % my_choice,
-                             reply_markup = hide_keyboard)
+                             reply_markup=hide_keyboard)
 
         if ((my_choice == "rock" and user_choice == "paper") or
             (my_choice == "paper" and user_choice == "scissors") or
@@ -116,6 +117,5 @@ class TelegramThread(eb_thread.Thread):
             self.bot.sendMessage(user, "You win!")
         else:
             self.bot.sendMessage(user, "I win!")
-        
+
         self.state = None
-        
