9af9a66984a55002e37867f5e32ba41a81fb5f1a
Christer Enfors <christer.enfors@gmail.com>
2017-10-17 15:57:27 +0200

    Only run doctest on specific files.

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-for file in *.py; do
+files_with_tests="eb_activity.py eb_cmds_loader.py"
+
+for file in $files_with_tests; do
     echo "Checking $file..."
     python3 -m doctest $file
 done
